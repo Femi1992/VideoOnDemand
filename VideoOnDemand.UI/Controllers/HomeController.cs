fc11a42d5f3b74346d45d130b8bbd56e4b704dd6
Femi <olufemidokun@hotmail.co.uk>
2018-10-16 11:43:25 +0100

    Login View Created

--- VideoOnDemand.UI/Controllers/HomeController.cs
+++ VideoOnDemand.UI/Controllers/HomeController.cs
@@ -17,7 +17,6 @@ namespace VideoOnDemand.UI.Controllers
 
         public IActionResult Index()
         {
-            //check if user is logged in
             if (!_signInManager.IsSignedIn(User))
                 return RedirectToAction("Login", "Account");
 
