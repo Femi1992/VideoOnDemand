6ce81e89f8a363f4f5f68d237b4a073a395150d7
Femi <olufemidokun@hotmail.co.uk>
2018-10-17 12:49:27 +0100

    Data transfer objects needed to display the data views created.

---
+++ VideoOnDemand.UI/Models/MembershipViewModels/CourseViewModel.cs
@@ -0,0 +1,13 @@
+﻿using VideoOnDemand.UI.Models.DTOModels;
+using System.Collections.Generic;
+
+
+namespace VideoOnDemand.UI.Models.MembershipViewModels
+{
+    public class CourseViewModel
+    {
+        public CourseDTO Course { get; set; }
+        public InstructorDTO Instructor { get; set; }
+        public IEnumerable<ModuleDTO> Modules { get; set; }
+    }
+}
---
+++ VideoOnDemand.UI/Models/MembershipViewModels/VideoViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using VideoOnDemand.UI.Models.DTOModels;
+
+namespace VideoOnDemand.UI.Models.MembershipViewModels
+{
+    public class VideoViewModel
+    {
+        public VideoDTO Video { get; set; }
+        public InstructorDTO Instructor { get; set; }
+        public CourseDTO Course { get; set; }
+        public LessonInfoDTO lessonInfo { get; set; }
+    }
+}
