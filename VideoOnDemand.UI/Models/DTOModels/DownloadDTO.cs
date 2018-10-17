f68f5360935deca7c7e22b4b825d86dbb9eb0bb5
Femi <olufemidokun@hotmail.co.uk>
2018-10-17 12:34:29 +0100

    Data transfer models created

---
+++ VideoOnDemand.UI/Models/DTOModels/DownloadDTO.cs
@@ -0,0 +1,9 @@
+﻿
+namespace VideoOnDemand.UI.Models.DTOModels
+{
+    public class DownloadDTO
+    {
+        public string DownloadUrl { get; set; }
+        public string DownloadTitle { get; set; }
+    }
+}
---
+++ VideoOnDemand.UI/Models/DTOModels/UserCourseDTO.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+
+namespace VideoOnDemand.UI.Models.DTOModels
+{
+    public class UserCourseDTO
+    {
+        [DisplayName("User ID")]
+        public string UserId { get; set; }
+
+        [DisplayName("Course Id")]
+        public int CourseId { get; set; }
+
+        [DisplayName("Email")]
+        public string UserEmail { get; set; }
+
+        [DisplayName("Title")]
+        public string CourseTitle { get; set; }
+    }
+}
